4f0c2a8ab542eb61873f8f96c836566cf3ce3c31
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-04-17 23:07:36 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -15,7 +15,7 @@ class TPQueue {
       throw std::string("Full");
     } else {
       int zam = last++;
-      while ((value.prior > arr[(zam - 1) % size].prior) && (last >= first) {
+      while ((value.prior > arr[(zam - 1) % size].prior) && (last >= first)) {
         arr[(1 + zam) % size] = arr[zam % size];
         zam--;
       }
